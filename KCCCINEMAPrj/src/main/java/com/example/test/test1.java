a9acdb5519c73a1feec8a680655184c2391bbdbc
movie7650 <movie7650@naver.com>
2023-07-13 12:16:54 +0900

    상여이 커밋했데이~

---
+++ KCCCINEMAPrj/src/main/java/com/example/test/test1.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+		test2 Test2 = new test2();
+		Test2.add(7, 8);
+	}
+
+}
---
+++ KCCCINEMAPrj/src/main/java/com/example/test/test2.java
@@ -0,0 +1,12 @@
+package com.example.test;
+
+public class test2 {
+
+	int a;
+	int b;
+
+	public static void  add(int a, int b) {
+		System.out.println(a+b);
+	}
+
+}

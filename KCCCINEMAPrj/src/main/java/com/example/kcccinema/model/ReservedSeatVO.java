07220ac0b103b31d6d2a2eb05e28b0e65bba818c
movie7650 <movie7650@naver.com>
2023-07-14 14:56:03 +0900

    [상영][#update] ReservedSeatVO 수정

--- KCCCINEMAPrj/src/main/java/com/example/kcccinema/model/ReservedSeatVO.java
+++ KCCCINEMAPrj/src/main/java/com/example/kcccinema/model/ReservedSeatVO.java
@@ -3,10 +3,8 @@ package com.example.kcccinema.model;
 import lombok.Data;
 
 @Data
-public class ReservedSeat {
+public class ReservedSeatVO {
 	private int scheduleId;
 	private int seatId;
 	private int ticketId;
-	
-
 }

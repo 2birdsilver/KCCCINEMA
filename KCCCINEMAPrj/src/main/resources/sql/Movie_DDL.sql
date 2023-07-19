ea019e5863746f754b73e2f5f7f300e862a5af70
KOSA <zoooz0616@gmail.com>
2023-07-19 09:46:09 +0900

    [은지][#add]sql문 두개로 나눔

--- KCCCINEMAPrj/src/main/resources/sql/Movie_DDL.sql
+++ KCCCINEMAPrj/src/main/resources/sql/Movie_DDL.sql
@@ -39,42 +39,13 @@ CREATE TABLE cinema (
 	cinema_location	varchar2(50)	NOT NULL,
 	cinema_phone	varchar2(20) );
 	
-INSERT INTO cinema
-VALUES (1, '혜화', '서울', '02-1111-2222');
- 
-INSERT INTO cinema
-VALUES (2, '울산', '울산', '052-1111-2222');
-    
-INSERT INTO cinema
-VALUES (3, '구미', '경북', '054-1111-2222');
-
-INSERT INTO cinema
-VALUES (4, '부천', '경기도', '031-1111-2222');
-
-INSERT INTO cinema
-VALUES (5, '인천', '인천', '032-1111-2222');
-
-
 CREATE TABLE time (
 	time_id NUMBER CONSTRAINT time_time_id_pk PRIMARY KEY,
 	start_time	DATE NOT NULL
 );
 
 
-ALTER SESSION SET NLS_DATE_FORMAT = 'HH24:MI:SS';
-
-INSERT INTO time values (1,'09:00:00');
-
-INSERT INTO time values (2,'11:00:00');
-
-INSERT INTO time values (3,'13:00:00');
-
-INSERT INTO time values (4,'15:00:00');
-
-INSERT INTO time values (5,'17:00:00');
-
-INSERT INTO time values (6,'19:00:00');
-
+ALTER SESSION SET NLS_DATE_FORMAT = 'HH24:MI:SS';	
 
 CREATE TABLE seat (
 	seat_id	NUMBER	CONSTRAINT seat_seat_id_pk PRIMARY KEY,
@@ -89,65 +60,6 @@ CREATE TABLE theater (
 	cinema_id	NUMBER	CONSTRAINT theater_cinema_id_fk REFERENCES cinema(cinema_id),
 	theater_no	NUMBER	NOT NULL
 );
-
-
-INSERT into theater
-VALUES
-	(1, 1, 1);
-    
-    INSERT INTO theater
-VALUES
-	(2, 1, 2);
-    
-    INSERT INTO theater
-VALUES
-	(3, 1, 3);
-    
-    INSERT INTO theater
-VALUES
-	(4, 2, 1);
-    
-    INSERT INTO theater
-VALUES
-	(5, 2, 2);
-    
-    INSERT INTO theater
-VALUES
-	(6, 2, 3);
-    
-    INSERT INTO theater
-VALUES
-	(7, 3, 1);
-    
-    INSERT INTO theater
-VALUES
-	(8, 3, 2);
-    
-    INSERT INTO theater
-VALUES
-	(9, 3, 3);
-    
-    INSERT INTO theater
-VALUES
-	(10, 4, 1);
-    
-    INSERT INTO theater
-VALUES
-	(11, 4, 2);
-    
-    INSERT INTO theater
-VALUES
-	(12, 4, 3);
-    
-    INSERT INTO theater
-VALUES
-	(13, 5, 1);
-    
-    INSERT INTO theater
-VALUES
-	(14, 5, 2);
-INSERT INTO theater 
-    VALUES (15, 5, 3);
 
 CREATE TABLE users (
     user_id VARCHAR2(20) NOT NULL PRIMARY KEY,
@@ -208,5 +120,3 @@ foreign key (seat_id)  references seat(seat_id);
 alter table reserved_seat 
 add constraint fk_seat_ticket_id
 foreign key (ticket_id)  references ticket(ticket_id);
-
-   
---
+++ KCCCINEMAPrj/src/main/resources/sql/Movie_DML.sql
@@ -0,0 +1,56 @@
+INSERT INTO cinema
+VALUES (1, '혜화', '서울', '02-1111-2222');
+ 
+INSERT INTO cinema
+VALUES (2, '울산', '울산', '052-1111-2222');
+    
+INSERT INTO cinema
+VALUES (3, '구미', '경북', '054-1111-2222');
+
+INSERT INTO cinema
+VALUES (4, '부천', '경기도', '031-1111-2222');
+
+INSERT INTO cinema
+VALUES (5, '인천', '인천', '032-1111-2222');
+
+INSERT INTO time values (1,'09:00:00');
+
+INSERT INTO time values (2,'11:00:00');
+
+INSERT INTO time values (3,'13:00:00');
+
+INSERT INTO time values (4,'15:00:00');
+
+INSERT INTO time values (5,'17:00:00');
+
+INSERT INTO time values (6,'19:00:00');
+
+INSERT into theater VALUES (1, 1, 1);
+    
+INSERT INTO theater VALUES (2, 1, 2);
+    
+INSERT INTO theater VALUES (3, 1, 3);
+    
+INSERT INTO theater VALUES (4, 2, 1);
+    
+INSERT INTO theater VALUES (5, 2, 2);
+    
+INSERT INTO theater VALUES (6, 2, 3);
+    
+INSERT INTO theater VALUES (7, 3, 1);
+    
+INSERT INTO theater VALUES (8, 3, 2);
+    
+INSERT INTO theater VALUES (9, 3, 3);
+    
+INSERT INTO theater VALUES (10, 4, 1);
+    
+INSERT INTO theater VALUES (11, 4, 2);
+    
+INSERT INTO theater VALUES (12, 4, 3);
+    
+INSERT INTO theater VALUES (13, 5, 1);
+    
+INSERT INTO theater VALUES (14, 5, 2);
+
+INSERT INTO theater VALUES (15, 5, 3);

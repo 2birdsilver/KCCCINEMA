fd146639e3bb811371e6ca0e4a670216ad24615f
KOSA <zoooz0616@gmail.com>
2023-07-19 16:38:56 +0900

    [은지][#update, #add]book.html 수정 및 파비콘 추가

--- KCCCINEMAPrj/src/main/java/com/example/kcccinema/controller/Book/BookingController.java
+++ KCCCINEMAPrj/src/main/java/com/example/kcccinema/controller/Book/BookingController.java
@@ -1,4 +1,4 @@
-package com.example.kcccinema.controller;
+package com.example.kcccinema.controller.Book;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

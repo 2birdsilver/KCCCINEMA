d39fcecf7377fb66f333e033e2e4a4bc0c581fe5
luenah <onlynforever@gmail.com>
2023-07-26 17:50:30 +0900

    [하늘][#add] 회원가입 기능 구현 완료

--- KCCCINEMAPrj/src/main/java/com/example/kcccinema/controller/users/UsersController.java
+++ KCCCINEMAPrj/src/main/java/com/example/kcccinema/controller/users/UsersController.java
@@ -3,6 +3,7 @@ package com.example.kcccinema.controller.users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,4 +62,22 @@ public class UsersController {
 		return "user/signup";
 	}
 
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String insertMember(UsersVO user, HttpSession session, Model model) {
+		try {
+			if (!user.getUserPassword().equals(user.getUserPassword2())) {
+				model.addAttribute("user", user);
+				model.addAttribute("message", "MEMBER_PW_RE");
+				return "user/signup";
+			}
+			usersService.insertMember(user);
+		} catch (DuplicateKeyException e) {
+			user.setUserId(null);
+			model.addAttribute("user", user);
+			model.addAttribute("message", "ID_ALREADY_EXIST");
+			return "user/signup";
+		}
+		session.invalidate();
+		return "redirect:/";
+	}
 }
